196beb4ee4a26b488e372d969f58c4e3c9601565
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-02-12 14:07:22 -0800

    Fix program interface

--- src/index.js
+++ src/index.js
@@ -1,43 +1,52 @@
 const { read } = require("./utils");
-const Trie = require("./Trie");
 const prompt = require("prompt-sync")({ sigint: true });
-
-const trie = new Trie();
 const words = read("./words.txt");
+const Trie = require("./Trie");
+const trie = new Trie();
+
+// Set Up Trie
 for (const word of words) trie.insert(word);
+console.log("--- TRIE READY ---");
 
-console.log("TRIE READY");
-
-let QUERY = "";
-let GRAY_CHARS = "";
-let YELLOW_CHARS = "";
-let CURRENT = 0;
-let OPTION = "";
+let wordleGreenCharacters = "";
+let wordleGrayCharacters = "";
+let wordleYellowCharacters = "";
+let userOption = "";
 // "q": "Reset Query"
 // "g": "Reset Gray Character Set"
 // "y": "Reset Green Charcter Set"
 
 do {
-	switch (OPTION) {
+	switch (userOption) {
 		case "q":
-			QUERY = prompt("Query: ");
+			wordleGreenCharacters = prompt("Query: ");
 			break;
 		case "g":
-			GRAY_CHARS = prompt("Characters Don't Exits (Grays): ");
+			wordleGrayCharacters = prompt("Characters Don't Exits (Grays): ");
 			break;
 		case "y":
-			YELLOW_CHARS = prompt("Characters Exist (Yellow): ");
+			wordleYellowCharacters = prompt("Characters Exist (Yellow): ");
 			break;
 		default:
-			QUERY = prompt("Query: ");
-			GRAY_CHARS = prompt("Characters Don't Exits (Grays): ");
-			YELLOW_CHARS = prompt("Characters Exist (Yellow): ");
+			wordleGreenCharacters = prompt("Query: ");
+			wordleGrayCharacters = prompt("Characters Don't Exits (Grays): ");
+			wordleYellowCharacters = prompt("Characters Exist (Yellow): ");
 	}
-	console.log(QUERY);
-	console.log(GRAY_CHARS);
-	console.log(YELLOW_CHARS);
-	const results = trie.search(QUERY, GRAY_CHARS, YELLOW_CHARS);
+	console.log("\n--- FILTERS ---")
+	console.log(wordleGreenCharacters);
+	console.log(wordleGrayCharacters || ".....");
+	console.log(wordleYellowCharacters || ".....");
+	const results = trie.search(
+		wordleGreenCharacters,
+		wordleGrayCharacters,
+		wordleYellowCharacters
+	);
+	console.log("\n--- RESULTS ---")
 	console.log(results);
-	OPTION = prompt("Option: ");
-	CURRENT++;
-} while (CURRENT < 10);
+	console.log("\n--- MENU ---")
+	userOption = prompt("Option: ");
+} while (userOption !== ":q");
+
+// Destory Trie
+for (const word of words) trie.remove(word);
+console.log("\n---TRIE DESTROYED---");
